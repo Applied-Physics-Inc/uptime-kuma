b87bbcb5fc0d818cb8ba7c4b71bb9093aa042ed0
0xShellcode <BDAthans@yahoo.com>
2024-03-19 09:40:22 -0500

    Updated uptime-kuma network

--- start-uptime-kuma.sh
+++ start-uptime-kuma.sh
@@ -8,7 +8,7 @@ if docker ps -a --format '{{.Names}}' | grep -q "^uptime-kuma$"; then
     echo "Container 'uptime-kuma' already exists. Starting the container."
     docker start uptime-kuma
 else
-    echo "Container 'uptime-kuma' does not exist. Running a new container."
-    docker run --hostname uptime-kuma --restart always -v uptime-kuma:/app/data --network nginx-rp --name uptime-kuma louislam/uptime-kuma:1
+    echo "Container 'uptime-kuma' not found. Running a new container."
+    docker run --hostname uptime-kuma --restart always -v uptime-kuma:/app/data --network uptime-kuma --name uptime-kuma louislam/uptime-kuma:1
 fi
 
